572044603b61b3a3543d01e014e97c32c3dc202d
vuduykhuong1412 <khuongvd.brainos@gmail.com>
2017-06-11 16:41:11 +0700

    Commit again L2

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'giongcaymoi.com');
+define('DB_NAME', 'giongcaymoi.com');  // Repair By KhuongVD
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'root');             // Repair By KhuongVD
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '');            // Repair By KhuongVD
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
